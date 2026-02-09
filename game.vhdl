b2251eeaff1191c7c51f9acaf7d0c17943dc073e
Pedro <pedrosouza7d4@gmail.com>
2026-02-09 10:25:54 -0300

    state machine draft

--- game.vhdl
+++ game.vhdl
@@ -1,6 +1,7 @@
 library IEEE;
 library work;
 use IEEE.std_logic_1164.all;
+use IEEE.std_logic_misc.all; -- Contains or_reduce
 use work.simon_game.all;
 
 entity game is port(
@@ -15,20 +16,62 @@ architecture arch of game is
   port (
     clk, enable, reset: in std_logic;
     sequence: out sequence_t;
-    sequence_len: out unsigned(4 downto 0);
     finished: out boolean
     );
   end component;
 
-  signal game_stage: game_stage_t := ASLEEP;
+  component game_st_mach is port (
+    clock: in std_logic;
+    player_input: in input_t;
+    sequence: in sequence_t;
+    sequence_finished: in std_logic;
+
+    wakeup: in std_logic;
+    teach_end: in std_logic;
+    
+    new_symbol: out std_logic;
+    reset_sequence: out std_logic;
+    stage: out game_stage_t
+    );
+  end component;
+
+  signal player_input: player_input_t;
   signal sequence: sequence_t;
+  signal wakeup: std_logic;
+  signal teach_end: std_logic;
+  
+  signal new_symbol: std_logic;
+  signal reset_sequence: std_logic;
+  signal stage: game_stage_t := ASLEEP;
 begin
   SEQ_GEN: sequence_generator port map (
     clk => clock,
-    enable => ,
-    reset => ,
-    sequence => ,
-    sequence_len => ,
-    finished => 
+    enable => new_symbol,
+    reset => reset_sequence,
+    sequence => sequence,
+    finished => sequence_finished
   );
+
+  ST_MACH: game_st_mach port map (
+    clock => clock,
+    player_input => player_input,
+    sequence => sequence,
+    sequence_finished => sequence_finished,
+
+    wakeup => wakeup,
+    teach_end => teach_end,
+
+    new_symbol => new_symbol,
+    reset_sequence => reset_sequence,
+    stage => stage
+  );
+
+  WAKEUP_GEN: wakeup <= or_reduce(buttons);
+  TEACH_END_GEN: teach_end <= '0'; -- FIXME
+  PLAYER_INPUT_MARSHALL: block begin
+    player_input.blue <= buttons(0);
+    player_input.yellow <= buttons(1);
+    player_input.green <= buttons(2);
+    player_input.red <= buttons(3);
+  end block;
 end architecture;
---
+++ game_st_mach.vhdl
@@ -0,0 +1,98 @@
+library IEEE;
+library work;
+use IEEE.std_logic_1164.all;
+use work.simon_game.all;
+
+-- game control signals come from this state machine
+entity game_st_mach is port (
+  clock: in std_logic;
+  player_input: in input_t;
+  sequence: in sequence_t;
+  sequence_finished: in std_logic;
+
+  wakeup: in std_logic;
+  teach_end: in std_logic;
+  
+  new_symbol: out std_logic;
+  reset_sequence: out std_logic;
+  stage: out game_stage_t
+);
+end entity;
+
+-- type game_stage_t is (ASLEEP, TEACH, TEST, PASS, FAIL)
+
+architecture behav of game_st_mach is
+  signal cur_stage, next_stage: game_stage_t := ASLEEP;
+  signal correct_symbol: sequence_item_t;
+  signal sequence_needle: unsigned(4 downto 0);
+  signal is_correct: std_logic;
+begin
+
+  FETCH_CORRECT: process (sequence, sequence_needle)
+  begin
+    correct_symbol <= sequence(sequence_needle);
+  end process;
+
+  INPUT_TEST: process (player_input, correct_symbol)
+  begin
+    with correct_symbol select is_correct <=
+      (player_input.blue = '1') when BLUE,
+      (player_input.yellow = '1') when YELLOW,
+      (player_input.green = '1') when GREEN,
+      (player_input.red = '1') when RED,
+      '0' when others;
+  end process;
+  
+  STAGE_REG_PROC: process (clock)
+  begin
+    if rising_edge(clock) then
+      cur_stage <= next_stage;
+    end if;
+  end process;
+
+  STAGE_TRANSITIONS: process
+    (cur_stage, player_input, wakeup, teach_end, sequence_finished, is_correct)
+  begin
+    case cur_stage is
+      when ASLEEP =>
+        new_symbol <= '0';
+        reset_sequence <= '0';
+        
+        if wakeup = '1'
+        then next_stage <= TEACH;
+        else next_stage <= ASLEEP;
+        end if;
+      when TEACH =>
+        new_symbol <= '0';
+        reset_sequence <= '0';
+
+        if teach_end = '1'
+        then next_stage <= TEST;
+        else next_stage <= TEACH;
+        end if;
+      when TEST =>
+        new_symbol <= '0';
+        reset_sequence <= '0';
+        
+        if (   player_input.blue
+            or player_input.yellow
+            or player_input.green
+            or player_input.red)
+        then
+          if (is_correct = '1')
+          then next_stage <= PASS;
+          else next_stage <= FAIL;
+          end if;
+        else next_stage <= TEST;
+        end if;
+      when PASS =>
+        new_symbol <= '1';
+        reset_sequence <= '0';
+        next_stage <= TEST;
+      when others =>
+        new_symbol <= '0';
+        reset_sequence <= '1';
+        next_stage <= ASLEEP;
+    end case;
+  end process;
+end architecture;
